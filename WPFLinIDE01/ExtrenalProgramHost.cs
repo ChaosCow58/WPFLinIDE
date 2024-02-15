e1c15058e57cc80104caed980a9623e4d8c9aa4e
ChaosCow58 <101840562+ChaosCow58@users.noreply.github.com>
2024-02-15 09:59:01 -0600

    Add project files.

---
+++ WPFLinIDE01/ExtrenalProgramHost.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+
+namespace WPFLinIDE01
+{
+    internal class ExtrenalProgramHost : HwndHost
+    {
+        private IntPtr _hwnd;
+
+        protected override HandleRef BuildWindowCore(HandleRef hwndParent)
+        {
+            Process externalProcess = new Process();
+            externalProcess.StartInfo.FileName = "powershell.exe";
+            externalProcess.Start();
+            externalProcess.WaitForInputIdle();
+
+            _hwnd = externalProcess.MainWindowHandle;
+
+            if (_hwnd == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("Failed to get the handle of the external process window.");
+            }
+
+            if (!NativeMethods.IsWindowVisible(_hwnd))
+            {
+                throw new InvalidOperationException("The external process window is not visible.");
+            }
+
+            NativeMethods.SetParent(_hwnd, hwndParent.Handle);
+            return new HandleRef(this, _hwnd);
+        }
+
+        protected override void DestroyWindowCore(HandleRef hwnd)
+        {
+            NativeMethods.SetParent(_hwnd, IntPtr.Zero);
+            _hwnd = IntPtr.Zero;
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            return new Size();
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            return finalSize;
+        }
+
+        private class NativeMethods
+        {
+            [DllImport("user32.dll", SetLastError = true)]
+            public static extern IntPtr SetParent(IntPtr hWnd, IntPtr hWndParent);
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+            public static extern bool IsWindowVisible(IntPtr hWnd);
+        }
+    }
+}
---
+++ WPFLinIDE01/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLinIDE01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            activeTermial();
+        }
+
+        private void activeTermial()
+        {
+          /*  Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = "powershell.exe",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    // CreateNoWindow = true,
+                }
+            };
+
+            process.Start();*/
+
+           // tbTermial.Text = process.StandardOutput.ReadToEnd();
+        }
+    }
+}
