2a4630f8b1ef5a6f27019364447b056dbc622bc7
ChaosCow58 <sryan@tryan.net>
2024-02-22 21:58:25 -0600

    Added powershell and key bindings

--- WPFLinIDE01/MainWindow.xaml.cs
+++ WPFLinIDE01/MainWindow.xaml.cs
@@ -1,49 +1,117 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFLinIDE01
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+
+        private Process process;
+        private WindowsFormsHost host;
+        private ConsoleControl.ConsoleControl terminal;
+
+        public ICommand Show_PowerShell { get; }
+
         public MainWindow()
         {
             InitializeComponent();
-            activeTermial();
+
+            Closing += MainWindow_Closing;
+            Loaded += MainWindow_Loaded;
+
+            Show_PowerShell = new RelayCommand(ShowPowerShell);
+
+            DataContext = this;
         }
 
-        private void activeTermial()
+        private void ShowPowerShell(object parameter)
         {
-          /*  Process process = new Process()
+            if (gTermialPanel.Visibility == Visibility.Collapsed)
             {
-                StartInfo = new ProcessStartInfo()
+                CreateTermial();
+                gTermialPanel.Visibility = Visibility.Visible;
+            }
+            else if (gTermialPanel.Visibility == Visibility.Visible)
+            {
+                gTermialPanel.Visibility = Visibility.Collapsed;
+                if (process != null && !process.HasExited)
                 {
-                    FileName = "powershell.exe",
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    RedirectStandardInput = true,
-                    // CreateNoWindow = true,
+                    process.Kill();
+                    terminal.Dispose();
                 }
+            }
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (process != null && !process.HasExited)
+            { 
+                process.Kill();
+                terminal.Dispose();
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreateTermial();
+        }
+
+        private void CreateTermial()
+        { 
+            process = new Process();
+            process.StartInfo = new ProcessStartInfo()
+            {
+                FileName = "powershell.exe",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
             };
 
-            process.Start();*/
+            terminal = new ConsoleControl.ConsoleControl();
 
-           // tbTermial.Text = process.StandardOutput.ReadToEnd();
+            process.Start();
+
+            terminal.ProcessInterface.StartProcess(process.StartInfo);
+            terminal.OnConsoleInput += Terminal_OnConsoleInput;
+
+
+            terminal.IsInputEnabled = true;
+            terminal.Font = new Font("Poppins", 11);
+
+
+            host = new WindowsFormsHost();
+            host.Child = terminal;
+
+            gTermialPanel.Children.Add(host);
         }
+
+        private void Terminal_OnConsoleInput(object sender, ConsoleControl.ConsoleEventArgs args)
+        {
+            if (args.Content == "exit")
+            {
+                // Close the terminal
+                Dispatcher.Invoke(() =>
+                {
+                    gTermialPanel.Children.Remove(host);
+                    gTermialPanel.Visibility = Visibility.Collapsed;
+                });
+
+                // Kill the process
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                    terminal.Dispose();
+                }
+            }
+        }
+
+
     }
 }
