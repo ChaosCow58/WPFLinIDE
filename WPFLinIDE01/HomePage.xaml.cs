01450f2ba54b410aa76e87719389029a11df4411
ChaosCow58 <101840562+ChaosCow58@users.noreply.github.com>
2024-02-23 10:13:14 -0600

    Added Homepage

---
+++ WPFLinIDE01/HomePage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace WPFLinIDE01
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void btOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog = new OpenFolderDialog()
+            {
+                Title = "Select a Project",
+                InitialDirectory = "C:\\"
+            };
+
+            if (openFolderDialog.ShowDialog() == true)
+            {
+                App.Current.Properties["ProjectPath"] = openFolderDialog.FolderName;
+                Debug.WriteLine(openFolderDialog.FolderName.Substring(0, openFolderDialog.RootDirectory.Length - 5));
+                App.Current.Properties["ProjectName"] = "";
+                Close();
+            }
+            
+        }
+
+        private void btNewProject_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFolderDialog openFolderDialog = new OpenFolderDialog()
+            {
+                Title = "Select a Project Location",
+                InitialDirectory = "C:\\"
+            };
+
+            NameProject project = new NameProject();
+            project.ShowDialog();
+
+            if (App.Current.Properties["ProjectName"] == null)
+            { 
+                return;
+            }
+
+            if (openFolderDialog.ShowDialog() == true)
+            {
+                DirectoryInfo diretoryPath = Directory.CreateDirectory(Path.Combine(openFolderDialog.FolderName, App.Current.Properties["ProjectName"].ToString()));
+                Directory.CreateDirectory(Path.Combine(diretoryPath.FullName, "Assets"));
+                Directory.CreateDirectory(Path.Combine(diretoryPath.FullName, "Resources"));
+
+                App.Current.Properties["ProjectPath"] = diretoryPath.FullName;
+                App.Current.Properties["ProjectName"] = diretoryPath.Name;
+
+                Close();
+            }
+           
+        }
+    }
+}
--- WPFLinIDE01/MainWindow.xaml.cs
+++ WPFLinIDE01/MainWindow.xaml.cs
@@ -1,25 +1,37 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Forms.Integration;
 using System.Windows.Input;
+using ConsoleControls = ConsoleControl.ConsoleControl;
 
 namespace WPFLinIDE01
 {
     public partial class MainWindow : Window
     {
+        HomePage homePage;
 
         private Process process;
         private WindowsFormsHost host;
-        private ConsoleControl.ConsoleControl terminal;
+        private ConsoleControls terminal;
 
         public ICommand Show_PowerShell { get; }
 
         public MainWindow()
         {
             InitializeComponent();
+
+            homePage = new HomePage();
+            homePage.ShowDialog();
+
+            if (App.Current.Properties["ProjectPath"] == null)
+            { 
+                Close();
+            }
 
             Closing += MainWindow_Closing;
             Loaded += MainWindow_Loaded;
@@ -29,12 +41,71 @@ namespace WPFLinIDE01
             DataContext = this;
         }
 
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (process != null && !process.HasExited)
+            {
+                process.Kill();
+                terminal.Dispose();
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            DisplayFileSystem();
+            CreateTermial();
+        }
+
+        #region FileExplorer
+
+        private void PopulateTreeView(string path, TreeViewItem parentNode)
+        {
+            try
+            {
+                foreach (string folder in Directory.GetDirectories(path))
+                {
+                    TreeViewItem folderNode = new TreeViewItem();
+                    folderNode.Header = Path.GetFileName(folder);
+                    parentNode.Items.Add(folderNode);
+
+                    PopulateTreeView(folder, folderNode);
+                }
+                foreach (string file in Directory.GetFiles(path))
+                { 
+                    TreeViewItem fileNode = new TreeViewItem();
+                    fileNode.Header = Path.GetFileName(file);
+                    parentNode.Items.Add(fileNode);
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                System.Windows.MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(1);    
+            }
+        }
+
+        private void DisplayFileSystem()
+        {
+            string rootFolder = App.Current.Properties["ProjectPath"].ToString();
+
+            TreeViewItem rootNode = new TreeViewItem();
+            rootNode.Header = App.Current.Properties["ProjectName"].ToString();
+            tvFileTree.Items.Add(rootNode);
+
+            PopulateTreeView(rootFolder, rootNode);
+        }
+
+        #endregion FileExplorer
+
+        #region Commands
         private void ShowPowerShell(object parameter)
         {
             if (gTermialPanel.Visibility == Visibility.Collapsed)
             {
                 CreateTermial();
                 gTermialPanel.Visibility = Visibility.Visible;
+                terminal.Focus();
             }
             else if (gTermialPanel.Visibility == Visibility.Visible)
             {
@@ -46,21 +117,9 @@ namespace WPFLinIDE01
                 }
             }
         }
+        #endregion Commands
 
-        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            if (process != null && !process.HasExited)
-            { 
-                process.Kill();
-                terminal.Dispose();
-            }
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            CreateTermial();
-        }
-
+        #region Terminal
         private void CreateTermial()
         { 
             process = new Process();
@@ -74,7 +133,7 @@ namespace WPFLinIDE01
                 RedirectStandardInput = true,
             };
 
-            terminal = new ConsoleControl.ConsoleControl();
+            terminal = new ConsoleControls();
 
             process.Start();
 
@@ -84,7 +143,10 @@ namespace WPFLinIDE01
 
             terminal.IsInputEnabled = true;
             terminal.Font = new Font("Poppins", 11);
+            
+            
 
+            terminal.BorderStyle = BorderStyle.FixedSingle;
 
             host = new WindowsFormsHost();
             host.Child = terminal;
@@ -111,6 +173,7 @@ namespace WPFLinIDE01
                 }
             }
         }
+        #endregion Terminal
 
 
     }
